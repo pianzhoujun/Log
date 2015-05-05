362fa4fc51da59b824c2807e071d59767cd3e6b8
wgshub <wgsmail@163.com>
2015-05-05 20:00:49 +0800

    update CLLoger

--- CLLogger.cpp
+++ CLLogger.cpp
@@ -2,14 +2,18 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <string.h>
-#include <stdio.h>
+#include <cstdio>
 #include <unistd.h>
+#include <cstdlib>
 
 CLLogger* CLLogger::m_pLog = NULL;
 
 CLLogger::CLLogger()
 {
         m_fd = open (LOG_FILE_NAME , O_RDWR|O_CREAT|O_APPEND,S_IRUSR|S_IWUSR);
+        m_nUsedBytesForBuffer = 0;
+        m_pLogBuffer = NULL;
+        m_bFlagForProcessExit = false;
 }
 
 
@@ -17,6 +21,12 @@ CLLogger* CLLogger::GetInstance()
 {
         if (!m_pLog) {
                 m_pLog = new CLLogger(); 
+                if (atexit (CLLogger::OnProcessExit) != 0) {
+                        if (m_pLog != NULL) {
+                                m_pLog->WriteLog("In CLLogger::GetInstance() , atexit error" , errno);
+                                m_pLog->Flush();
+                        }
+                }
         }
         return m_pLog;
 }
@@ -37,6 +47,38 @@ CLStatus CLLogger::WriteLog (const char* pstrMsg , long lErrorCode)
 {
         if (pstrMsg == NULL || strlen(pstrMsg) == 0 || m_fd == -1)
                 return CLStatus(-1 , 0);
+        unsigned int nleftroom = BUFFER_SIZE_LOG_FILE - m_nUsedBytesForBuffer;
+        unsigned int len_strmsg = strlen(pstrMsg);
+        char buf[MAX_SIZE];
+        snprintf(buf , MAX_SIZE , "Error Code:%ld\n",lErrorCode);
+        unsigned int len_code = strlen(buf);
+        int total_len = len_strmsg + len_code;
+        if (total_len > BUFFER_SIZE_LOG_FILE || m_bFlagForProcessExit) {
+                Flush();
+                if (m_fd == -1)
+                        return CLStatus (-1 , 0);
+                size_t r = write (m_fd , pstrMsg , len_strmsg);
+                if (r == -1)
+                        return CLStatus (-1 , 0);
+                r = write (m_fd , buf , len_code);
+                if (r == -1)
+                        return CLStatus (-1 , 0);
+                return CLStatus (0 , 0);
+        }
+
+        if (total_len > nleftroom) {
+                CLStatus s = Flush();
+                if (!s.IsSuccess()) {
+                        return CLStatus(-1 , 0);
+                }
+        }
+
+        memcpy (m_pLogBuffer + m_nUsedBytesForBuffer , pstrMsg , len_strmsg);
+        m_nUsedBytesForBuffer += len_strmsg;
+        memcpy (m_pLogBuffer + m_nUsedBytesForBuffer , buf , len_code);
+        m_nUsedBytesForBuffer += len_code;
+        return CLStatus (0 , 0);
+#if 0
         size_t r = write (m_fd , pstrMsg , sizeof(pstrMsg));
         if (r == -1)
                 return CLStatus(-1 ,errno);
@@ -47,4 +89,32 @@ CLStatus CLLogger::WriteLog (const char* pstrMsg , long lErrorCode)
                 return CLStatus(-1 , errno);
         else
                 return CLStatus(0 , 0);
+#endif
 }
+
+CLStatus CLLogger::Flush()
+{
+        if (m_fd == -1 || m_pLogBuffer == NULL)
+                return CLStatus (-1 , 0);
+
+        if (m_nUsedBytesForBuffer == 0)
+                return CLStatus (0 , 0);
+
+        size_t r = write (m_fd , m_pLogBuffer , m_nUsedBytesForBuffer);
+        if (r == -1)
+                return CLStatus (-1 , 0);
+        else
+                return CLStatus (0 , 0);
+
+}
+
+void CLLogger::OnProcessExit()
+{
+        CLLogger *pLog = CLLogger::GetInstance();
+        if (!pLog) {
+                pLog->Flush();
+                pLog->m_bFlagForProcessExit = true;
+        }
+}
+
+
--- CLLogger.h
+++ CLLogger.h
@@ -4,18 +4,27 @@
 
 #define LOG_FILE_NAME   "CLLogger.txt"
 #define MAX_SIZE        256
+#define BUFFER_SIZE_LOG_FILE    4096
 
 class CLLogger {
        public:
                static CLLogger* GetInstance();
                CLStatus WriteLogMsg(const char* pstrMsg , long lErrorCode);
                CLStatus WriteLog(const char *pstrMsg , long lErrorCode);
+               CLStatus Flush();
                CLLogger(CLLogger&) = delete;
                CLLogger& operator= (CLLogger&) = delete;
+
        private:
                CLLogger();
+               ~CLLogger();
+               static void OnProcessExit();
+
        private:
                static CLLogger *m_pLog;
                int m_fd;
+               char *m_pLogBuffer;
+               unsigned int m_nUsedBytesForBuffer;
+               bool m_bFlagForProcessExit;               
 };
 #endif
